172a15dd38b2d3e8508ef3ecb47e1a97f60b6fdf
YagamiLight126 <fuzhicheng94@gmail.com>
2020-04-03 16:57:54 +0800

    chore: 更新模板仓库地址

--- templates/index.js
+++ templates/index.js
@@ -1,11 +1,11 @@
 module.exports = {
   all: ['react-ts', 'react-js'],
   'react-ts': {
-    url: 'https://github.com/YagamiLight126/C.C.ts.git',
+    url: 'https://github.com/YagamiLight126/C.C.tsx.git',
     branch: 'master',
   },
   'react-js': {
-    url: 'https://github.com/YagamiLight126/react-js-template.git',
+    url: 'https://github.com/YagamiLight126/C.C.jsx.git',
     branch: 'master',
   },
 }
